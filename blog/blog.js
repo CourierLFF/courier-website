f45cef900f96cd8f075ab5e4ee9a430057be3c51
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-12-04 17:29:15 -0500

    create unloadCategory function

--- blog/blog.js
+++ blog/blog.js
@@ -1,6 +1,47 @@
-const categories = document.querySelectorAll('.blog-category');
+const blogView = document.querySelector('.blog-view');
+let categories = document.querySelectorAll('.blog-category');
+const blogHeader = document.querySelector('#blog-header');
 
-for (let i = 0; i < categories.length; i++) {
+function loadCategory(categoryName) {
+    console.log("Loading category: " + categoryName);
+}
+
+function unloadCategory() {
+    blogHeader.innerText = "Blog";
+    blogView.innerHTML = '';
+
+    blogView.appendChild(document.createElement('div')).classList.add('blog-categories');
+    
+    const generalCategory = document.createElement('div');
+    generalCategory.classList.add('blog-category');
+    generalCategory.id = "general-articles-category";
+    generalCategory.innerHTML = '<img src="assets/images/folder-icon-closed.png"><p>General Articles</p>';
+    blogView.querySelector('.blog-categories').appendChild(generalCategory);
+
+    const writeUpsCategory = document.createElement('div');
+    writeUpsCategory.classList.add('blog-category');
+    writeUpsCategory.id = "write-ups-category";
+    writeUpsCategory.innerHTML = '<img src="assets/images/folder-icon-closed.png"><p>CTF Write-Ups</p>';
+    blogView.querySelector('.blog-categories').appendChild(writeUpsCategory);
+
+    const miscCategory = document.createElement('div');
+    miscCategory.classList.add('blog-category');
+    miscCategory.id = "miscellaneous-ramblings-category";
+    miscCategory.innerHTML = '<img src="assets/images/folder-icon-closed.png"><p>Miscellaneous Ramblings</p>';
+    blogView.querySelector('.blog-categories').appendChild(miscCategory);
+
+    const mysteryCategory = document.createElement('div');
+    mysteryCategory.classList.add('blog-category');
+    mysteryCategory.id = "mystery-category";
+    mysteryCategory.innerHTML = '<img src="assets/images/folder-icon-closed.png"><p>???</p>';
+    blogView.querySelector('.blog-categories').appendChild(mysteryCategory);    
+
+    categories = document.querySelectorAll('.blog-category');
+    bindEvents();
+}
+
+function bindEvents() {
+    for (let i = 0; i < categories.length; i++) {
         categories[i].addEventListener('mouseenter', () => {
             const category_folder_img = categories[i].querySelector('img');
             category_folder_img.src = "assets/images/folder-icon.png";
@@ -18,6 +59,10 @@ for (let i = 0; i < categories.length; i++) {
         });
 
         categories[i].addEventListener('click', () => {
-            alert("Blog posts coming soon!");
+            const categoryName = categories[i].querySelector('p').innerText;
+            loadCategory(categoryName);
         });
     }
+}
+
+bindEvents();
