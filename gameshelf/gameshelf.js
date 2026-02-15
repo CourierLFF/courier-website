d70664969675c4e746c2c317e45fe89a994a0798
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2026-02-15 09:53:53 -0500

    Style games on gameshelf

--- gameshelf/gameshelf.js
+++ gameshelf/gameshelf.js
@@ -12,9 +12,15 @@ console.log(gameShelfData)
 
 const gameshelfTable = document.getElementById('gameshelf-table');
 gameShelfData.forEach(game => {
-    const row = gameshelfTable.insertRow();
-    const nameCell = row.insertCell(0);
-    const ratingCell = row.insertCell(1);
-    nameCell.innerHTML = `<a href="${game.igdb_url}">${game.name}</a>`;
-    ratingCell.textContent = game.user_rating;
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    const rating = document.createElement('td');
+    
+    name.innerHTML = `<img class="cover-art" src="${game.cover_art}" alt="${game.name}"><a class="game-link" href="${game.igdb_url}">${game.name}</a>`;
+    rating.textContent = game.user_rating;
+
+    row.appendChild(name);
+    row.appendChild(rating);
+    row.classList.add('game-row');
+    gameshelfTable.appendChild(row);
 });
