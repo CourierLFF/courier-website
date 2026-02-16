1c747ddacf879440b6b36f7c36254efa22c6f21b
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2026-02-16 11:07:28 -0500

    Let the state buttons actually filter the games

--- gameshelf/gameshelf.js
+++ gameshelf/gameshelf.js
@@ -25,7 +25,6 @@ function disableOtherButtons() {
 }
 
 let gameShelfDataPlaying = gameShelfData.filter(game => game.game_state === 'Playing');
-let gameShelfDataBacklog = gameShelfData.filter(game => game.game_state === 'Backlog');
 let gameShelfDataCompleted = gameShelfData.filter(game => game.game_state === 'Completed');
 let gameShelfDataDropped = gameShelfData.filter(game => game.game_state === 'Dropped');
 
@@ -33,21 +32,62 @@ for (const button of document.querySelectorAll('.tracked-state-buttons button'))
     button.addEventListener('click', () => {
         currentState = button.textContent;
         disableOtherButtons();
+        updateGameShelfTable();
     });
 }
         
 
 const gameshelfTable = document.getElementById('gameshelf-table');
-gameShelfData.forEach(game => {
-    const row = document.createElement('tr');
-    const name = document.createElement('td');
-    const rating = document.createElement('td');
-    
-    name.innerHTML = `<img class="cover-art" src="${game.cover_art}" alt="${game.name}"><a class="game-link" href="${game.igdb_url}">${game.name}</a>`;
-    rating.textContent = game.user_rating;
 
-    row.appendChild(name);
-    row.appendChild(rating);
-    row.classList.add('game-row');
-    gameshelfTable.appendChild(row);
-});
+function updateGameShelfTable() {
+    gameshelfTable.innerHTML = '<tr><th>Game</th><th>Rating</th></tr>';
+    switch (currentState) {
+        case 'Playing':
+            gameShelfDataPlaying.forEach(game => {
+                const row = document.createElement('tr');
+                const name = document.createElement('td');
+                const rating = document.createElement('td');
+                
+                name.innerHTML = `<img class="cover-art" src="${game.cover_art}" alt="${game.name}"><a class="game-link" href="${game.igdb_url}">${game.name}</a>`;
+                rating.textContent = game.user_rating;
+
+                row.appendChild(name);
+                row.appendChild(rating);
+                row.classList.add('game-row');
+                gameshelfTable.appendChild(row);
+            });
+            break;
+        case 'Completed':
+            gameShelfDataCompleted.forEach(game => {
+                const row = document.createElement('tr');
+                const name = document.createElement('td');
+                const rating = document.createElement('td');
+                
+                name.innerHTML = `<img class="cover-art" src="${game.cover_art}" alt="${game.name}"><a class="game-link" href="${game.igdb_url}">${game.name}</a>`;
+                rating.textContent = game.user_rating;
+
+                row.appendChild(name);
+                row.appendChild(rating);
+                row.classList.add('game-row');
+                gameshelfTable.appendChild(row);
+            });
+            break;
+        case 'Dropped':
+            gameShelfDataDropped.forEach(game => {
+                const row = document.createElement('tr');
+                const name = document.createElement('td');
+                const rating = document.createElement('td');
+                
+                name.innerHTML = `<img class="cover-art" src="${game.cover_art}" alt="${game.name}"><a class="game-link" href="${game.igdb_url}">${game.name}</a>`;
+                rating.textContent = game.user_rating;
+
+                row.appendChild(name);
+                row.appendChild(rating);
+                row.classList.add('game-row');
+                gameshelfTable.appendChild(row);
+            });
+            break;
+    }  
+}
+
+updateGameShelfTable();
